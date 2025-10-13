36a29a714418f92f4795063bfdb3f3edc21382df
jaewon <jaewon0077@naver.com>
2025-10-14 01:40:04 +0900

    match_code

--- src/api/matches.routes.js
+++ src/api/matches.routes.js
@@ -1,1 +1,18 @@
-// 
+//
+const express = require('express');
+const matchController = require('../controllers/match.controller')
+const router = express.Router();
+
+//1.2.7 푸드뱅크 -> 매치 요청 조회
+router.get('/list',matchController.getPendingMatches);
+
+//1.2.10 푸드뱅크 -> 자신에게 할당된 매치 상세정보 조회
+router.get('/list/accepted',matchController.getAcceptedMatches);
+
+//1.2.8 푸드뱅크 -> 매치 수락
+router.get('/accept',matchController.acceptMatch);
+
+//1.2.9 푸드뱅크 ->매치 거절
+router.get('/reject',matchController.rejectMatch);
+
+module.exports = router;
